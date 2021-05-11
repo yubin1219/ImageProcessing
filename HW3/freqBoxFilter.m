965dccf3116e4db43f86112f34128a67e68c23a8
Yubin-Cho <74402562+yubin1219@users.noreply.github.com>
2021-05-11 23:11:40 +0900

    Update freqBoxFilter.m

--- HW3/freqBoxFilter.m
+++ HW3/freqBoxFilter.m
@@ -2,40 +2,62 @@ function dst = freqBoxFilter(src,kernel_size)
   
 % Frequency domain Filtering - Using Box kernel %
   
-h = ones(kernel_size,kernel_size) ./ (kernel_size^2);
+h = ones(kernel_size,kernel_size) ./ (kernel_size^2);  % h(x,y)
+
 M = size(src,1);
 N = size(src, 2);
 hp = zeros(2*M, 2*N);
-hp(M - (kernel_size-1)/2 : M + (kernel_size-1)/2, N - (kernel_size-1)/2 : N + (kernel_size-1)/2) = h(:,:);
-H = fftshift(fft2(hp));
+hp(M - (kernel_size-1)/2 : M + (kernel_size-1)/2, N - (kernel_size-1)/2 : N + (kernel_size-1)/2) = h(:,:);  % hp(x,y)
+
+H = fftshift(fft2(hp));                                % H(u,v)
+
 logf_b = log(1+abs(H));
-fmax_b = max(logf_b(:));
-H_show = uint8(255*(logf_b/fmax_b)); % H(u,v)
-figure();imshow(H_show);
+fm = logf_b - min(logf_b(:));
+fmax_b = max(fm(:));
+H_show = uint8(255*(fm/fmax_b));
+figure();imshow(H_show);                % kernel Fourier spectrum 출력
 
-img_p = zeros(2*M, 2*N);
+ph = angle(H);
+phm = ph-min(ph(:));
+phmax = max(phm(:));
+ps = uint8(255*(phm/phmax));
+figure();imshow(ps)                     % kernel phase 출력
+
 % padding
+img_p = zeros(2*M, 2*N);
 img_p(1:M,1:N) = src(:,:);
-F = fftshift(fft2(img_p));
-logf = log(1+abs(F));
-fmax = max(logf(:));
-F_show = uint8(255*(logf/fmax));     % F(u,v)
-figure();imshow(F_show);
+F = fftshift(fft2(img_p));              % F(u,v)
 
-G = H.*F;                            % G(u,v)
+logf = log(1+abs(F));
+fm = logf - min(logf(:));
+fmax = max(fm(:));
+F_show = uint8(255*(fm/fmax));
+figure();imshow(F_show);                % Input image Fourier spectrum 출력
+
+pf = angle(F);
+pfm = pf-min(pf(:));
+pfmax = max(pfm(:));
+pfs = uint8(255*(pfm/pfmax));
+figure();imshow(pfs)                    % input image phase 출력
+
+G = H.*F;                               % G(u,v)
+
 logfG = log(1+abs(G));
-fmaxG = max(logfG(:));
-G_show = uint8(255*(logfG/fmaxG));
+fm = logfG - min(logfG(:));
+fmaxG = max(fm(:));
+G_show = uint8(255*(fm/fmaxG));
 figure();imshow(G_show);
 
 G_ifft = ifft2(G);
-gp = ifftshift(G_ifft);              % gp(x,y)
+gp = ifftshift(G_ifft);                 % gp(x,y)
+
 fp = abs(gp);
-fpmax = max(fp(:));
-g_p = uint8(255*(fp/fpmax));
+fpm = fp - min(fp(:));
+fpmax = max(fpm(:));
+g_p = uint8(255*(fpm/fpmax));
 figure(); imshow(g_p)
 
-g = g_p(1:M,1:N);                    % g(x,y)
+g = g_p(1:M,1:N);                       % g(x,y)
 figure(); imshow(g)
 
 dst = g;
